663c1bb758d516191a08be69607d49176a803a5b
RinR1n <le6edev2307@yandex.ru>
2026-04-11 17:29:16 +0300

    Add game_detail screen, implement data displaying

--- django_project/games/urls.py
+++ django_project/games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import game_list
+from .views import game_list, game_detail
 
 urlpatterns = [
     path('', game_list, name='game_list'),
+    path('<int:pk>/', game_detail, name='game_detail'),
 ]
--- django_project/games/views.py
+++ django_project/games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 def game_list(request):
@@ -6,4 +6,11 @@ def game_list(request):
 
     return render(request, 'games/game_list.html', {
         'games': games
+    })
+
+def game_detail(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+
+    return render(request, 'games/game_detail.html', {
+        'game': game
     })
