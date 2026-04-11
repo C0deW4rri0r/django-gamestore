4471521d9332bb5d104e3a005705e9ceba2719f8
RinR1n <le6edev2307@yandex.ru>
2026-04-11 18:07:02 +0300

    implement CRUD

--- django_project/games/urls.py
+++ django_project/games/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import game_list, game_detail
+from .views import game_list, game_detail, game_create, game_update, game_delete
 
 urlpatterns = [
     path('', game_list, name='game_list'),
     path('<int:pk>/', game_detail, name='game_detail'),
+    path('create/', game_create, name='game_create'),
+    path('<int:pk>/edit/', game_update, name='game_update'),
+    path('<int:pk>/delete/', game_delete, name='game_delete'),
 ]
--- django_project/games/views.py
+++ django_project/games/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import GameForm
 from .models import Game
 
 def game_list(request):
@@ -12,5 +14,49 @@ def game_detail(request, pk):
     game = get_object_or_404(Game, pk=pk)
 
     return render(request, 'games/game_detail.html', {
+        'game': game
+    })
+
+@login_required
+def game_create(request):
+    if request.method == 'POST':
+        form = GameForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('game_list')
+    else:
+        form = GameForm()
+
+    return render(request, 'games/game_form.html', {
+        'form': form,
+        'title': 'Добавить игру'
+    })
+
+@login_required
+def game_update(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+
+    if request.method == 'POST':
+        form = GameForm(request.POST, request.FILES, instance=game)
+        if form.is_valid():
+            form.save()
+            return redirect('game_detail', pk=pk)
+    else:
+        form = GameForm(instance=game)
+
+    return render(request, 'games/game_form.html', {
+        'form': form,
+        'title': 'Редактировать игру'
+    })
+
+@login_required
+def game_delete(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+
+    if request.method == 'POST':
+        game.delete()
+        return redirect('game_list')
+
+    return render(request, 'games/game_confirm_delete.html', {
         'game': game
     })
