39c5922ae1e0fd80b5e9112c675115059327313d
RinR1n <le6edev2307@yandex.ru>
2026-04-22 21:38:28 +0300

    fix media root

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -133,7 +133,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = PERSISTENT_DIR / 'media'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/games/'
